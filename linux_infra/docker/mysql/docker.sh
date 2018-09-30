0a68b4b7f5e9f187f387fc68aa75a337a6afea83
ebonggu <zztownsi@gmail.com>
2018-10-01 00:33:13 +0900

    Update docker.sh

--- linux_infra/docker/mysql/docker.sh
+++ linux_infra/docker/mysql/docker.sh
@@ -16,3 +16,11 @@ $name --character-set-server=utf8 --collation-server=utf8_unicode_ci
 echo "====== $name run  ====="
 
 
+
+docker run --restart=always --name mysql -p 3306:3306 -v /var/lib/mysql:/c/docker/mysql/data -v /etc/mysql/conf.d:/c/docker/mysql/conf.d -e MYSQL_ROOT_PASSWORD=1234 -d mysql:8.0.12 --character-set-server=utf8 --collation-server=utf8_unicode_ci 
+
+docker cp mysql:/etc/mysql/conf.d mysql/conf.d
+docker cp mysql:/var/lib/mysql mysql/data
+
+docker-machine ip
+
