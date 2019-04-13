2d7728c87d62156f9b2b31e3ffd8c0ae3e21767f
zztownsi <zztownis@gmail.com>
2019-04-13 21:14:04 +0900

    111

--- java/EmptyFoloderRemove.java
+++ java/EmptyFoloderRemove.java
@@ -4,15 +4,14 @@ import java.io.File;
 
 public class EmptyFoloderRemove {
 
-//	private static final String FOLDER_LOCATION = "G:\\\\정리";
-	private static final String FOLDER_LOCATION = "D:\\투디스크 다운로드";
+	private static final String FOLDER_LOCATION = "G:\\\\정리";
 	private static boolean isFinished = false;
 
 	private static void replaceText(String fileLocation) {
-		
+
 		File folder = new File(fileLocation);
 		File[] listofFiles = folder.listFiles();
-		
+
 		if (listofFiles.length == 0) {
 			System.out.println("Folder Name :: " + folder.getAbsolutePath() + " is deleted.");
 			folder.delete();
@@ -20,9 +19,44 @@ public class EmptyFoloderRemove {
 		} else {
 			for (int j = 0; j < listofFiles.length; j++) {
 				File file = listofFiles[j];
+
+				String fileNm = file.getName();
+
 				if (file.isDirectory()) {
-					System.out.println(" getAbsolutePath >> " + folder.getAbsolutePath());
+//					System.out.println(" getAbsolutePath >> " + folder.getAbsolutePath());
 					replaceText(file.getAbsolutePath());
+				} else {
+					if (fileNm.lastIndexOf(".") > -1) {
+						String exn = fileNm.substring(fileNm.lastIndexOf(".") + 1).toLowerCase();
+//					System.out.println("fileNm ->" + fileNm  + " exn >> " + exn);
+//						System.out.println(exn);
+
+						//지우는 대상 확장자
+						if (exn.equals("jpg") || exn.equals("png") || exn.equals("txt") || exn.equals("smi") || exn.equals("db")) {
+
+							if (exn.equals("smi")) {
+								if(folder.listFiles().length == 1) {
+									System.out.println(file.getAbsolutePath() + " is deleted.");
+									file.delete();
+									System.out.println("Folder Name :: " + folder.getAbsolutePath() + " is deleted.");
+									folder.delete();
+								}
+							}else {
+								System.out.println(file.getAbsolutePath() + " is deleted.");
+								file.delete();
+							}
+						} 
+						
+						 
+						if (exn.equals("mp4") && exn.equals("avi")) {
+//							System.out.println("안 삭제 exn >> " + exn + " >> " + file.getAbsolutePath());
+						}
+					
+						
+					} else {
+						System.out.println(file.getAbsolutePath() + " is deleted.");
+						file.delete();
+					}
 				}
 			}
 		}
