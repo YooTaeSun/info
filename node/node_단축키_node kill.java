17e98dfb8bf86793cb1416c190b59810976179dd
ebonggu <zztownsi@gmail.com>
2019-07-17 17:43:17 +0900

    Create node_단축키_node kill.java

---
+++ node/node_단축키_node kill.java
@@ -0,0 +1,22 @@
+vscode 단축키
+
+편집’ > ‘파일에서 찾기’
+Ctrl + Shift + F (디렉토리 전체에서 원하는 문자열을 검색)
+Ctrl + Shift + K
+Ctrl +  K + F (정렬)
+
+명령어 패널 열기 : Ctrl + Shift + p
+빠른 열기 : Ctrl + P
+토글 사이등바 : Ctrl +B
+
+
+----------------------
+node kill
+
+netstat -ano | findstr :5000
+taskkill /PID 5000 /F
+
+-------------------------
+
+typescript
+tsc person  (.ts -> .js)
