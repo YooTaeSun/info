010ac0774f9030005c91aa7d0100563907fb9fdc
ebonggu <zztownsi@gmail.com>
2019-07-17 17:39:12 +0900

    node

---
+++ node/node debug.java
@@ -0,0 +1,19 @@
+/.vscode/launch.json 
+
+{
+    // Use IntelliSense to learn about possible attributes.
+    // Hover to view descriptions of existing attributes.
+    // For more information, visit: https://go.microsoft.com/fwlink/?linkid=830387
+    "version": "0.2.0",
+    "configurations": [
+        {
+            "type": "node",
+            "request": "launch",
+            "name": "Launch Program",
+            "program": "${workspaceFolder}\\server.js"
+        }
+    ]
+}
+
+debug√¢ø°º≠ 
+C:\Program Files\nodejs\node.exe --inspect-brk=18344 server.js 
